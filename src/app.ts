cc7e6689df84a97cce8cab3117c62ff1b21120c7
Ciro Nunes <ciroanunes@gmail.com>
2015-12-06 11:59:00 +0000

    feat: setup router

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,13 @@
+import {provide} from 'angular2/core';
 import {bootstrap} from 'angular2/platform/browser';
+import {
+  ROUTER_PROVIDERS,
+  HashLocationStrategy,
+  LocationStrategy
+} from 'angular2/router';
+
 import {Angular2PlaygroundApp} from './app/angular2-playground';
 
 
-bootstrap(Angular2PlaygroundApp);
+bootstrap(Angular2PlaygroundApp, [
+  ROUTER_PROVIDERS, provide(LocationStrategy, { useClass: HashLocationStrategy })]);
---
+++ src/app/components/hello/hello.ts
@@ -0,0 +1,16 @@
+import {Component} from 'angular2/core';
+
+
+@Component({
+  selector: 'hello',
+  templateUrl: 'app/components/hello/hello.html',
+  styleUrls: ['app/components/hello/hello.css'],
+  providers: [],
+  directives: [],
+  pipes: []
+})
+export class HelloCmp {
+
+  constructor() {}
+
+}
